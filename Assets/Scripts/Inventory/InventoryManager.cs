3945cdd108e2178e4982a2ae05b65af70e07afca
PaReng2 <korreppsj@naver.com>
2025-10-13 15:20:08 +0900

    [Feat] 인벤토리 제작

---
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager Instance;
+
+    [Header("Inventory Setting")]
+    public int inventorySize = 20;
+    public GameObject inventoryUI;
+    public Transform itemSlotParent;
+    public GameObject itemSlotPrefab;
+
+    [Header("Input")]
+    public KeyCode inventoryKey = KeyCode.I;
+    private List<ItemSlot> slots = new List<ItemSlot> ();
+    private bool isInventoryOpen = false;
+
+    private void Awake()
+    {
+        if(Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateInventorySlots();
+        inventoryUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(inventoryKey))
+        {
+            ToggleInventory();
+        }
+    }
+
+    void CreateInventorySlots()
+    {
+        for (int i = 0; i < inventorySize; i++)
+        {
+            GameObject slotObj = Instantiate(itemSlotPrefab, itemSlotParent);
+            ItemSlot slot = slotObj.GetComponent<ItemSlot>();
+            slots.Add(slot);
+        }
+    }
+
+    public void ToggleInventory()
+    {
+        isInventoryOpen = !isInventoryOpen;
+        inventoryUI.SetActive(isInventoryOpen);
+
+        if (isInventoryOpen)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    public bool AddItem(ItemData item, int amount = 1)
+    {
+        foreach (ItemSlot slot in slots)
+        {
+            if (slot.item == item && slot.amount < item.maxStack)
+            {
+                int SpaceLeft = item.maxStack - slot.amount;
+                int amountToAdd = Mathf.Min(amount, SpaceLeft);
+                slot.AddAmount(amountToAdd);
+                amount -= amountToAdd;
+
+                if (amount <= 0)
+                {
+                    return true;
+                }
+            }
+        }
+
+        foreach (ItemSlot slot in slots)
+        {
+            if (slot.item == null)
+            {
+                slot.SetItem(item, amount);
+                return true;
+            }
+        }
+
+        Debug.Log("인벤토리가 가득 참");
+        return false;
+    }
+
+    public void Removeitem(ItemData item, int amount = 1)
+    {
+        foreach (ItemSlot slot in slots)
+        {
+            if (slot.item == item)
+            {
+                slot.RemoveAmount(amount);
+                return;
+            }
+        }
+    }
+
+    public int GetItemCount(ItemData item)
+    {
+        int count = 0;
+        foreach (ItemSlot slot in slots)
+        {
+            if (slot.item == item)
+            {
+                count += slot.amount;
+            }
+        }
+        return count;   
+    }
+}
---
+++ Assets/Scripts/Inventory/ItemSlot.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemSlot : MonoBehaviour
+{
+    public ItemData item;
+    public int amount;
+
+    [Header("UI Referens")]
+    public Image itemIcon;
+    public Text amountText;
+    public GameObject emptySlotImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateSlotUI();
+    }
+
+    public void SetItem(ItemData newItem, int newAmount)
+    {
+        item = newItem;
+        amount = newAmount;
+        UpdateSlotUI();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddAmount(int value)
+    {
+        amount += value;
+        UpdateSlotUI();
+    }
+
+    public void RemoveAmount(int value)
+    {
+        amount -= value;
+
+        if (amount <= 0)
+        {
+            ClearSlot();
+        }
+        else
+        {
+            UpdateSlotUI();
+        }
+    }
+
+    public void ClearSlot()
+    {
+        item = null;
+        amount = 0;
+        UpdateSlotUI();
+
+    }
+
+    void UpdateSlotUI()
+    {
+        if (item != null)
+        {
+            itemIcon.sprite = item.itemIcon;
+            itemIcon.enabled = true;
+
+            amountText.text = amount > 1 ? amount.ToString() : "";
+            if (emptySlotImage != null)
+            {
+                emptySlotImage.SetActive(false);
+            }
+        }
+        else
+        {
+            itemIcon.enabled=false;
+            amountText.text = "";
+            if (emptySlotImage != null)
+            {
+                emptySlotImage.SetActive(true);
+            }
+        }
+    }
+}
